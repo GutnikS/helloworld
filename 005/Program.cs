11a7ae92d5fc85925fd6c327285a99b425b9d0b5
Gutnik <cipoga@gmail.com>
2023-07-08 12:31:26 +0300

    005

---
+++ 005/Program.cs
@@ -0,0 +1,13 @@
+﻿// приветствие пользователя в зависимости от имени
+Console.Write ("Как тебя зовут? ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "алиса") //оператор выравнивания шрифта
+{
+    Console.WriteLine ("Привет Алиса. Будешь чай?");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}

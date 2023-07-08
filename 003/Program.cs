89c90d049ac42ee77581eca1ec5e6aab4d6219b6
Gutnik <cipoga@gmail.com>
2023-07-08 12:07:52 +0300

    random

--- 003/Program.cs
+++ 003/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int numberA = 3;
-int numberB = 5;
+int numberA = new Random().Next(1,10); //случайное число от 1 до 10
+Console.WriteLine (numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine (numberB);
 Console.WriteLine (numberA + numberB);
-

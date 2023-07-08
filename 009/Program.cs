3f04da7c8cbe6be8955e8a7602595842cb194a34
Gutnik <cipoga@gmail.com>
2023-07-08 16:47:27 +0300

    заполнение массива случайными числами и блуждение по массиву

---
+++ 009/Program.cs
@@ -0,0 +1,27 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(23,350);
+        index++;
+    }
+}
+
+void PrintArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Console.WriteLine(collection[index]);
+        index++;
+    }
+}
+
+int [] array = new int [15]; //создай новый массив из 10 элементов. каждый элемент = 0
+FillArray(array);
+PrintArray(array);
+
+

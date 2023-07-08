31f460d52ef9645a5f3b854d25ddb4a562b1cd39
Gutnik <cipoga@gmail.com>
2023-07-08 16:15:04 +0300

    function1

--- 008/Program.cs
+++ 008/Program.cs
@@ -7,9 +7,14 @@
 }
 int a1=213, b1=13, c1=132, a2=12333, b2=35, c2=324, a3=76, b3=185, c3=55;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max (max1, max2, max3);
+//вариант записи поочередного поиска по 3шт максимальных, потом итоговый поиск
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max (max1, max2, max3);
+
+//вариант записи напрямую в функцию 
+
+int max = Max (Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
 
 Console.WriteLine(max);
